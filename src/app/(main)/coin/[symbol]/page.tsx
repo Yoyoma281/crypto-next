d40f6ad4f33bf65391567a2758c241200fbcb0b5
yoyoma281 <shai239@gmail.com>
2026-03-09 19:36:47 +0200

    .

--- src/app/(main)/coin/[symbol]/page.tsx
+++ src/app/(main)/coin/[symbol]/page.tsx
@@ -1,22 +1,27 @@
-// app/.../[symbol]/page.tsx
+// src/app/(main)/coin/[symbol]/page.tsx
+
 import { InternalApiFetch } from "@/app/api/ApiFetch";
 import CoinOverviewClient from "./components/chart/chart";
 import { BinanceKline } from "@/app/types";
 
-export default async function Page({
-  params,
-  searchParams,
-}: {
-  params: { symbol: string };
-  searchParams?: { interval?: string };
-}) {
+type PageProps = {
+  params: {
+    symbol: string;
+  };
+  searchParams?: {
+    interval?: string;
+  };
+};
+
+export default async function Page({ params, searchParams }: PageProps) {
   const symbol = params.symbol;
   const interval = searchParams?.interval ?? "1m";
-  console.log("SYMBOOOL: ", symbol);
 
-  const res = await InternalApiFetch.get(
-    `/coin/${symbol}?interval=${interval}`
-  ) as { data: BinanceKline[] };
+  console.log("SYMBOOOL:", symbol);
+
+  const res = (await InternalApiFetch.get(
+    `/coin/${symbol}?interval=${interval}`,
+  )) as { data: BinanceKline[] };
 
   return (
     <CoinOverviewClient
